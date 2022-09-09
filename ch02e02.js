7c7d21ab33ffbc7268bcde5bac327b9aa2c6bd85
awdeveloper00 <73925209+awdeveloper00@users.noreply.github.com>
2022-09-09 21:38:37 +0500

    chapter2 assignments
    chapter2 assignments

---
+++ ch02e02.js
@@ -0,0 +1,13 @@
+let name = "John Doe";
+let age = 25;
+let canICode = true;
+
+console.log(
+  "Hello, my name is " +
+    name +
+    ", I am " +
+    age +
+    " years old and I " +
+    (canICode ? "can" : "can not") +
+    " code JavaS"
+);
