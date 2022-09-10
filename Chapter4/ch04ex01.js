6031c104e04a74c36a9d3594f901ec4187a26e44
Hasham <hasham87@hotmail.com>
2022-09-10 18:07:27 +0500

    chapter 4 first assignment

---
+++ Chapter4/ch04ex01.js
@@ -0,0 +1,21 @@
+// Create a variable with a Boolean value
+let rain = true;
+
+// Output the value of the variable to the console
+console.log(rain);
+
+// Check whether the variable is true and if so, output a message to the
+// console
+
+if(rain){
+    console.log("It rained today");
+} 
+
+// Add another if statement with an ! in front of the variable to check
+// whether the condition is not true, and create a message that will be
+// printed to the console in that instance. You should have two if
+// statements, one with an ! and the other without. You could also use
+// an if and an else statement instead—experiment!
+else if (!rain) {
+    console.log("It did not rained today");   
+}
